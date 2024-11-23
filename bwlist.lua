9c54e6babc30078f51a7af33bbe79c13545c7fa6
VANKApidarnoi <lastnigra@gmail.com>
2024-11-23 09:01:16 +0200

    Create bwlist.lua

---
+++ bwlist.lua
@@ -0,0 +1,17 @@
+return {
+ 
+['white']={
+'bebraphone_pon',
+'skeletikostismert123',
+'morphill_q',
+'yfoffufn8755',
+'j99gl3m7wh',
+'pe12ik',
+'acesvn'
+},
+ 
+['black']={
+'pedro_712345'
+},
+ 
+}
