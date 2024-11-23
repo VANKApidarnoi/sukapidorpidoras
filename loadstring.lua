2f98aed4d7633134b71c678a82984dfa8a1bb431
VANKApidarnoi <lastnigra@gmail.com>
2024-11-23 18:20:49 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -4,6 +4,7 @@
 -->> execute again to reload script <<------------------------------------------------------------------------------------|
 -->> github: https://github.com/VANKApidarnoi/sukapidorpidoras/ <<--------------------------------------------------------|
 -->> report bugs / kicks on github ^ or vanya_zabivnoy666 (discord) <<----------------------------------------------------|
+-->> releases: https://github.com/VANKApidarnoi/sukapidorpidoras/releases <<----------------------------------------------|
 -->> ping can affect your farming speed <<--------------------------------------------------------------------------------|
 --[[------------------------------------------------------------------------------------------------------------------]]--|
 loadstring(game:HttpGet('https://raw.githubusercontent.com/VANKApidarnoi/sukapidorpidoras/refs/heads/main/vmain.lua'))()--|
