da52a9c89e75bd807c757a7ecc8aff8bb6e9126a
VANKApidarnoi <lastnigra@gmail.com>
2024-11-23 11:22:23 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -3,5 +3,6 @@
 > DONT SPAM EXECUTE BUTTON
 > execute again to reload script
 > github: https://github.com/VANKApidarnoi/sukapidorpidoras/
+> report bugs on github ^ or vanya_zabivnoy666 (discord)
 -----------------------------------------------------------]]--
 loadstring(game:HttpGet('https://pastebin.com/raw/c8GpftBQ'))()
