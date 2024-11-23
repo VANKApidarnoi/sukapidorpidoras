25ac40cb3426b3f73b8c4b12dff69ddc68e41ade
VANKApidarnoi <lastnigra@gmail.com>
2024-11-23 10:51:29 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,7 +1,7 @@
---[[------------------------------------------------------------
+--[[-----------------------------------------------------------
 > after executing wait ~3-10 seconds
 > DONT SPAM EXECUTE BUTTON
 > execute again to reload script
 > github: https://github.com/VANKApidarnoi/sukapidorpidoras/
-------------------------------------------------------------]]--
+-----------------------------------------------------------]]--
 loadstring(game:HttpGet('https://pastebin.com/raw/c8GpftBQ'))()
